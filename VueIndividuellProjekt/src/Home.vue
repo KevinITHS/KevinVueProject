e60431c47975ee983151aa6f4877778914d9bb16
KevinITHS <kevin.ali@iths.se>
2023-08-13 13:47:25 +0200

    .

--- VueIndividuellProjekt/src/Home.vue
+++ VueIndividuellProjekt/src/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="home">
-    <header><h1>Welcome to MMO World!</h1></header>
-    <body>
+  <header>
+    <div class="home">
+      <h1>Welcome to MMO World!</h1>
+
       <h2>Latest news, giveaways, video games information and more!</h2>
       <p>
         Explore the world of video games like never before with our diverse
         range of content.
       </p>
-    </body>
-  </div>
+    </div>
+  </header>
+
+  <body></body>
+
+  <footer></footer>
 </template>
 
 <script>
@@ -20,4 +25,26 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.home {
+  text-align: center;
+  margin: auto;
+  max-width: 800px;
+}
+
+.home h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.home h2 {
+  font-size: 18px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.home p {
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+</style>
